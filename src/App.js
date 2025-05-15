fc26f1c9af9159b8fddbe3a4e133984b802c606b
Ommama Habib <habibomama629@gmail.com>
2025-05-15 19:45:20 +0500

    added file uploader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from 'react';
+import FileUploader from './components/file_uploader';
+
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '40px' }}>
       <h1>Welcome to My React App on AWS!</h1>
       <p>This app is hosted using AWS S3 and CloudFront (Free Tier)</p>
+
+      <div style={{ marginTop: '40px' }}>
+        <h1>React + AWS S3 Upload</h1>
+        <FileUploader />
+      </div>
     </div>
   );
 }
